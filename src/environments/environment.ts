33148bc20f22c1f64c6ead4649c37b5f0f8ca184
deezdipan <dipanbaral@users.noreply.github.com>
2026-04-10 14:35:02 -0400

    Add environment files for Vercel build

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://evyunqkamewltmoduoeh.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2eXVucWthbWV3bHRtb2R1b2VoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMjI1NTcsImV4cCI6MjA4OTg5ODU1N30.nHc1Nf_UvAlBx8xijp_tebVkU1WjXpDcsKAWG4toL1U'
+};
